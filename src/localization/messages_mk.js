108a7043f26d968ee62270b2c3424dc639f13540
silovski <aleksandar.silovski@gmail.com>
2015-12-03 14:55:47 +0100

    Create messages_mk.js

---
+++ src/localization/messages_mk.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: MK (Macedonia; македонски јазик)
+ */
+$.extend( $.validator.messages, {
+	required: "Ова поле е задолжително",
+	remote: "Ова поле треба да се поправи",
+	email: "Внесете важечка e-mail обратница.",
+	url: "Внесете важечки URL.",
+	date: "Внесете важечки датум",
+	dateISO: "Внесете важечки датум (ISO).",
+	number: "Внесете правилен број",
+	digits: "Внесувајте само бројки",
+	creditcard: "Внесете точен број на кредитната картичка.",
+	equalTo: "Внесете повторно иста вредност.",
+	extension: "Внесете вредност со правилна наставка.",
+	maxlength: $.validator.format( "Максималниот број знаци е {0} ." ),
+	minlength: $.validator.format( "Минималниот број знаци е {0} ." ),
+	rangelength: $.validator.format( "Внесете вредност помеѓу {0} и {1} знаци." ),
+	range: $.validator.format( "Внесете вредност помеѓу {0} и {1}." ),
+	max: $.validator.format( "Внесете вредност помала или еднаква на {0}." ),
+	min: $.validator.format( "Внесете вредност поголема или еднаква на {0}." )
+} );
